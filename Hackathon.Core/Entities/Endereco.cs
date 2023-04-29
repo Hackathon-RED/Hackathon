74fc080ddd150e37fc50ec70348ca634b7c2fdc1
Leonardo Zanaro <leonardo.zanaro@gamasti.com.br>
2023-04-29 14:50:12 -0300

    Initial

---
+++ Hackathon.Core/Entities/Endereco.cs
@@ -0,0 +1,30 @@
+﻿using Hackathon.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Core.Entities
+{
+    public class Endereco : Entity
+    {
+        public string Rua { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+
+        public Endereco(string rua, string cidade, string estado, string cep)
+        {
+            Rua = rua;
+            Cidade = cidade;
+            Estado = estado;
+            Cep = cep;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rua}, {Cidade}, {Estado}, {Cep}";
+        }
+    }
+}
---
+++ Hackathon.Core/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Hackathon.Core.Common;
+
+namespace Hackathon.Core.Entities
+{
+    public class Usuario : Entity
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string ComprovanteResidencia { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public Guid EnderecoId { get; set; }
+        public virtual Endereco Endereco { get; set; }
+    }
+}
