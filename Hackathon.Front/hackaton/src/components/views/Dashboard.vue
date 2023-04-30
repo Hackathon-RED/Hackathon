22a0b3d38feffab889e2ab7df82795776446cf64
Leonardo Zanaro <leonardo.zanaro@gamasti.com.br>
2023-04-30 01:07:51 -0300

    Ajuste

--- Hackathon.Front/hackaton/src/components/views/Dashboard.vue
+++ Hackathon.Front/hackaton/src/components/views/Dashboard.vue
@@ -1,14 +1,41 @@
 <template>
     <div style="overflow-y: hidden;">
         <div style="display: flex; flex-wrap: wrap; justify-content: space-between; padding: 15px 30px;">
-        <div class="card" style="background-color: #028A0F;">
-            <h3>R$ 14.900,00</h3>
+        <div class="card" style="justify-content: start;">
+            <div style="max-width: 100px;"> 
+                <img style="width: 100%;" src="./../../assets/img/dog.svg">
+            </div>
+            <div class="card-title">
+                <p>Animais no abrigo</p>
+                <h3 class="title">294</h3>
+            </div>
         </div>
-        <div class="card" style="background-color: #028A0F;">
-            <h3>R$ 14.900,00</h3>
+        <div class="card" style="justify-content: start;">
+            <div style="max-width: 100px;"> 
+                <img style="width: 100%;" src="./../../assets/img/friends.svg">
+            </div>
+            <div class="card-title">
+                <p>Animais adotados do mês</p>
+                <h3 class="title">37</h3>
+            </div>
         </div>
-        <div class="card" style="background-color: #028A0F;">
-            <h3>R$ 14.900,00</h3>
+        <div class="card" style="justify-content: start;">
+            <div style="max-width: 100px;"> 
+                <img style="width: 100%;" src="./../../assets/img/money.svg">
+            </div>
+            <div class="card-title">
+                <p>Animais adotados do mês</p>
+                <h3 class="title">R$ 3.500,00</h3>
+            </div>
+        </div>
+        <div class="card" style="justify-content: start; background-color: #005438;">
+            <div style="max-width: 100px;"> 
+                <img style="width: 100%; filter:brightness(10)" src="./../../assets/img/cat.svg">
+            </div>
+            <div class="card-title">
+                <p style="color: white;">Solicitações</p>
+                <h3 class="title" style="color: white !important">294</h3>
+            </div>
         </div>
 
         <div class="card" style="width: 40%;">
@@ -17,7 +44,7 @@
         <div class="card" style="width: 50%; height: 350px;">
             <div style="display: flex; flex-direction: column; width: 100%; height: 100%;">
                 <div style="height: 15%;">
-                    <h3 style="color: #131313;">Últimas Solicitações</h3>
+                    <h3 style="color: #005438;">Últimas Solicitações</h3>
                 </div>
                 <div class="lista-solicitacoes">
                     <ul>
@@ -28,54 +55,63 @@
                                     <img style="width: 100%; border-radius: 15px;" src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg" />
                                 </div>
                                 <div style="margin-left: 15px;">
-                                    <h5>Bob</h5>
+                                    <h5>Bob <i class="fa-solid fa-mars"></i></h5>
                                     <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p style="font-size: 0.85em; opacity: .75; font-weight: bold;">Pug</p>
                                 </div>
                             </div>
                             <div style="margin-right: 15px;">
-                                <i class="fa-solid fa-mars"></i>
                                 <span>05/05/2023</span>
                             </div>
                         </div>
                        </li>
                        <li>
-                        <div style="display: flex;">
-                            <div style="max-width: 100px;">
-                                <img style="width: 100%; border-radius: 15px;" src="https://s2.glbimg.com/nvjFq8VRjyrpdQqaOeywz-5DFwY=/e.glbimg.com/og/ed/f/original/2021/08/27/captura_de_tela_2021-08-27_as_11.01.15.png" />
+                        <div style="display: flex; justify-content: space-between;">
+                            <div style="display: flex;">
+                                <div style="max-width: 100px;">
+                                    <img style="width: 100%; border-radius: 15px;" src="https://s2.glbimg.com/nvjFq8VRjyrpdQqaOeywz-5DFwY=/e.glbimg.com/og/ed/f/original/2021/08/27/captura_de_tela_2021-08-27_as_11.01.15.png" />
+                                </div>
+                                <div style="margin-left: 15px;">
+                                    <h5>João <i class="fa-solid fa-mars"></i></h5>
+                                    <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p style="font-size: 0.85em; opacity: .75; font-weight: bold;">Dachshund</p>
+                                </div>
                             </div>
-                            <div>
-                                <h5>Bob <span>Macho</span></h5>
-                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div>
+                            <div style="margin-right: 15px;">
                                 <span>05/05/2023</span>
                             </div>
                         </div>
                        </li>
                        <li>
-                        <div style="display: flex;">
-                            <div style="max-width: 100px;">
-                                <img style="width: 100%; border-radius: 15px;" src="https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg" />
+                        <div style="display: flex; justify-content: space-between;">
+                            <div style="display: flex;">
+                                <div style="max-width: 100px;">
+                                    <img style="width: 100%; border-radius: 15px;" src="https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg" />
+                                </div>
+                                <div style="margin-left: 15px;">
+                                    <h5>Lulu <i class="fa-solid fa-venus"></i></h5>
+                                    <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p style="font-size: 0.85em; opacity: .75; font-weight: bold;">Siamês</p>
+                                </div>
                             </div>
-                            <div>
-                                <h5>Lulu <span>Fêmea</span></h5>
-                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div>
+                            <div style="margin-right: 15px;">
                                 <span>05/05/2023</span>
                             </div>
                         </div>
                        </li>
                        <li>
-                        <div style="display: flex;">
-                            <div style="max-width: 100px;">
-                                <img style="width: 100%; border-radius: 15px;" src="https://static1.patasdacasa.com.br/articles/9/18/99/@/8911-conheca-mais-sobre-o-corgi-essa-raca-su-articles_media_mobile-2.jpg" />
+                        <div style="display: flex; justify-content: space-between;">
+                            <div style="display: flex;">
+                                <div style="max-width: 100px;">
+                                    <img style="width: 100%; border-radius: 15px;" src="https://static1.patasdacasa.com.br/articles/9/18/99/@/8911-conheca-mais-sobre-o-corgi-essa-raca-su-articles_media_mobile-2.jpg" />
+                                </div>
+                                <div style="margin-left: 15px;">
+                                    <h5>Maria <i class="fa-solid fa-venus"></i></h5>
+                                    <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p style="font-size: 0.85em; opacity: .75; font-weight: bold;">Corgi</p>
+                                </div>
                             </div>
-                            <div>
-                                <h5>Maria <span>Fêmea</span></h5>
-                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div>
+                            <div style="margin-right: 15px;">
                                 <span>05/05/2023</span>
                             </div>
                         </div>
@@ -89,6 +125,15 @@
 </template>
 
 <style scoped>
+
+.fa-venus {
+    color: #E75480;
+}
+
+.fa-mars {
+    color: #016064;
+}
+
 .card h5, .card p {
     margin: 0;
 }
@@ -97,7 +142,7 @@
     background-color: white;
     box-shadow: 1px 1px 10px 1px rgba(0,0,0,0.35);
     border-radius: 15px;
-    width: 25%;
+    width: 20%;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -130,6 +175,22 @@
 
 .lista-solicitacoes ul li p, .lista-solicitacoes ul li span {
     font-family: Arial, Helvetica, sans-serif;
+}
+
+.card-title { 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+
+.title { 
+    color: #005438 !important;
+    width: 100%;
+    text-align: center;
+    font-size: 2em;
+    margin: 0;
 }
 
 </style>
