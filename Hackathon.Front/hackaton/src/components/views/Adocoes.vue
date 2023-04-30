53a2016282e8659b2dcd6a8d67b9ed73c7f2c8f3
rykoji <ryan_koji@hotmail.com>
2023-04-30 00:03:33 -0300

    ajuste

--- Hackathon.Front/hackaton/src/components/views/Adocoes.vue
+++ Hackathon.Front/hackaton/src/components/views/Adocoes.vue
@@ -1,14 +1,181 @@
 <template>
     <div class="card-adocoes">
         <img class="icon-doacao"/>
-        <i class="fa-solid fa-network-wired"></i>
         <div class="title-cadastro">
             <i class="fa-solid fa-virus-slash"></i>ADOÇÕES
         </div>
 
         <div class="lista-solicitacoes">
-                    <ul>
+                    <ul class="aa">
                        <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://s2.glbimg.com/nvjFq8VRjyrpdQqaOeywz-5DFwY=/e.glbimg.com/og/ed/f/original/2021/08/27/captura_de_tela_2021-08-27_as_11.01.15.png" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Lulu <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://static1.patasdacasa.com.br/articles/9/18/99/@/8911-conheca-mais-sobre-o-corgi-essa-raca-su-articles_media_mobile-2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Maria <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                        </li>
+                        <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://s2.glbimg.com/nvjFq8VRjyrpdQqaOeywz-5DFwY=/e.glbimg.com/og/ed/f/original/2021/08/27/captura_de_tela_2021-08-27_as_11.01.15.png" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Lulu <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://static1.patasdacasa.com.br/articles/9/18/99/@/8911-conheca-mais-sobre-o-corgi-essa-raca-su-articles_media_mobile-2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Maria <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://s2.glbimg.com/nvjFq8VRjyrpdQqaOeywz-5DFwY=/e.glbimg.com/og/ed/f/original/2021/08/27/captura_de_tela_2021-08-27_as_11.01.15.png" />
+                            </div>
+                            <div>
+                                <h5>Bob <span>Macho</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Lulu <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                       </li>
+                       <li>
+                        <div style="display: flex;">
+                            <div style="max-width: 100px;">
+                                <img style="width: 100%; border-radius: 15px;" src="https://static1.patasdacasa.com.br/articles/9/18/99/@/8911-conheca-mais-sobre-o-corgi-essa-raca-su-articles_media_mobile-2.jpg" />
+                            </div>
+                            <div>
+                                <h5>Maria <span>Fêmea</span></h5>
+                                <p style="font-size: 0.85em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            <div>
+                                <span>05/05/2023</span>
+                            </div>
+                        </div>
+                        </li>
+                        <li>
                         <div style="display: flex;">
                             <div style="max-width: 100px;">
                                 <img style="width: 100%; border-radius: 15px;" src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_900x506.jpg" />
@@ -70,6 +237,13 @@
 </template>
 
 <style scoped>
+
+.aa{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+}
+
 
 
 .icon-doacao{
