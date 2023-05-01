2cb2d4c112ed6d4cc6e64e735d3fdf50d3cb6c33
Leonardo Zanaro <109481530+leonardo-zanaro@users.noreply.github.com>
2023-05-01 19:04:39 -0300

    Ajustes

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,21 @@
 using Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
+using HappyPet.Infrastructure.Data.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    var env = hostingContext.HostingEnvironment;
+    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+});
+
+DatabaseConfiguration.ConfiguraBanco(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
 
 var app = builder.Build();
 
---
+++ Infrastructure/Data/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data.Context;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HappyPet.Infrastructure.Data.Configuration
+{
+    public static class DatabaseConfiguration
+    {
+        public static void ConfiguraBanco(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("HappyPet.Infrastructure"));
+            });
+
+            // Adicionar aqui outras configurações dos repositórios, se houver
+            services.AddScoped<IVeterinarioRepository>(provider => (IVeterinarioRepository)provider.GetService<DataContext>());
+        }
+    }
+}
