789c8786729820711e8587f7d9e527f2d6988fe7
Leonardo Zanaro <109481530+leonardo-zanaro@users.noreply.github.com>
2023-05-01 09:51:27 -0300

    Ajustes

--- Hackathon.Core/Entities/Usuario.cs
+++ Hackathon.Core/Entities/Usuario.cs
@@ -1,19 +1,29 @@
 ﻿using Hackathon.Core.Entities.Common;
 using Hackathon.Core.Extensions;
+using Hackathon.Core.ValueObjects;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hackathon.Core.Entities
 {
     public class Usuario : Entity
     {
+        #region -> Propriedades
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
-        public CPF Cpf { get; set; }
         public string Rg { get; set; }
-        public string ComprovanteResidencia { get; set; }
-        public DateTime? DataNascimento { get; set; }
-        public string Email { get; set; }
+        public CPF Cpf { get; set; }
+        public Email Email { get; set; }
         public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        #endregion
+
+        #region -> Relacionamento
+        public Guid ComprovanteResidenciaId { get; set; }
+        public virtual Midia ComprovanteResidencia { get; set; }
+        [ForeignKey("Endereco")]
         public Guid EnderecoId { get; set; }
         public virtual Endereco Endereco { get; set; }
+        #endregion
     }
 }
---
+++ Hackathon.Core/ValueObjects/Email.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Hackathon.Core.ValueObjects
+{
+    public class Email
+    {
+        private readonly string _value;
+        private const string EmailPattern = @"^(?=.{1,256})([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+\.)*[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@(?:(?!.*[^.]{64,})(?:(?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+(?:[a-zA-Z]{2,}|xn--[a-zA-Z0-9]{2,}))$"; // Regex pattern para e-mail
+
+        public Email() { }
+        public Email(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("E-mail não pode ser nulo ou vazio");
+            }
+
+            // Verifica se o valor passado é um email válido usando Regex
+            if (!Regex.IsMatch(value, EmailPattern))
+            {
+                throw new ArgumentException("E-mail está com um formato inválido.");
+            }
+
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        public static implicit operator string(Email email)
+        {
+            return email._value;
+        }
+
+        public static explicit operator Email(string value)
+        {
+            return new Email(value);
+        }
+    }
+}
