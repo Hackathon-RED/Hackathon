10dafa766404dfc58b568aaacd8a0a4afe66f33b
Leonardo Zanaro <109481530+leonardo-zanaro@users.noreply.github.com>
2023-05-01 14:46:26 -0300

    Ajuste nos campos

---
+++ Hackathon.Core/Entities/VeterinarioEspecialidade.cs
@@ -0,0 +1,18 @@
+﻿using Hackathon.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Core.Entities
+{
+    public class VeterinarioEspecialidade : Entity
+    {
+        [ForeignKey("Especialidade")]
+        public Guid EspecialidadeId { get; set; }
+        [ForeignKey("Veterinario")]
+        public Guid VeterinarioId { get; set; }
+    }
+}
